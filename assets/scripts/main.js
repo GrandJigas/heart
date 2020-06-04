458844720800394618ff15f0bd5ef6dd7129a5e2
GrandJigas <dimail.ru@mail.ru>
2020-06-04 21:15:09 +0300

    script defer

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,6 +14,6 @@ window.onload = function () {
 function include(url) {
   var script = document.createElement("script");
   script.src = url;
-  script.async = true;
+  script.defer = true;
   document.getElementsByTagName("head")[0].appendChild(script);
 }
